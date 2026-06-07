c5ad3bb96ee3bfc11402dac667d64fa4f066dc8a
Dan Kortschak <dan@kortschak.io>
2026-06-08 08:52:22 +0930

    cmd/watcher/extensions: update shell-version array and fix instructions

--- cmd/watcher/extensions/user-activity@kortschak.io/install.bash
+++ cmd/watcher/extensions/user-activity@kortschak.io/install.bash
@@ -3,6 +3,6 @@
 cd $(dirname "$0")
 name=user-activity-kortschak.io.v$(jq -c '.version' <metadata.json).shell-extension.zip
 zip ${name} extension.js metadata.json
-gnome-extensions install ${name}
+gnome-extensions install ${name} --force
 
-echo 'Please restart your GNOME session and run `gnome-extensions enable user-activity-kortschak.io` to make the extension available.'
+echo 'Please restart your GNOME session and run `gnome-extensions enable user-activity@kortschak.io` to make the extension available.'
